28b0bc0f1515e7e233fa4192e853500dca8dc168
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-04-15 19:06:43 -0300

    :lock: (auth) redirect to sign in if not logged

--- src/layouts/app.tsx
+++ src/layouts/app.tsx
@@ -1,18 +1,40 @@
 import { Header } from "@/components/header";
-import type { ReactNode } from "react";
+import { api } from "@/lib/axios";
+import { useNavigate } from "@tanstack/react-router";
+import { isAxiosError } from "axios";
+import { useEffect, type ReactNode } from "react";
 
 interface AppLayoutProps {
 	children: ReactNode;
 }
 
 export function AppLayout({ children }: AppLayoutProps): JSX.Element {
-		return (
-			<div className="flex min-h-screen flex-col antialiased">
-				<Header />
+	const navigate = useNavigate();
 
-				<div className="flex flex-1 flex-col gap-4 p-8 py-6">
-					{children}
-				</div>
-			</div>
+	useEffect(() => {
+		const interceptorId = api.interceptors.response.use(
+			(response) => response,
+			(error) => {
+				if (isAxiosError(error)) {
+					const status = error.response?.status;
+					const code = error.response?.data.code;
+					console.log(error, "<>");
+					if (status === 401 && code === "UNAUTHORIZED")
+						navigate({ to: "/sign-in", replace: true });
+				}
+			},
 		);
-	}
+
+		return () => {
+			api.interceptors.response.eject(interceptorId);
+		};
+	}, [navigate]);
+
+	return (
+		<div className="flex min-h-screen flex-col antialiased">
+			<Header />
+
+			<div className="flex flex-1 flex-col gap-4 p-8 py-6">{children}</div>
+		</div>
+	);
+}
