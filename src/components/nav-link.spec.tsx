c906f346563d947a0c509aa2e7bae08e60f19f11
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-04-20 00:32:04 +1200

    test: using describe

--- src/components/nav-link.spec.tsx
+++ src/components/nav-link.spec.tsx
@@ -6,33 +6,34 @@ import {
 	createRootRoute,
 	createRoute,
 	createRouter,
-	redirect,
 } from "@tanstack/react-router";
 import { render } from "@testing-library/react";
 
-import { it } from "bun:test";
+import { describe, it } from "bun:test";
 
-it("should highlight when the nav link when is the current page link", async () => {
-	const rootRoute = createRootRoute({ component: () => <Outlet /> });
-	const componentRoute = createRoute({
-		path: "/",
-		getParentRoute: () => rootRoute,
-		component: () => <NavLink to='/about'>index to about</NavLink>,
+describe("NavLink", () => {
+	it("should highlight when the nav link when is the current page link", async () => {
+		const rootRoute = createRootRoute({ component: () => <Outlet /> });
+		const componentRoute = createRoute({
+			path: "/",
+			getParentRoute: () => rootRoute,
+			component: () => <NavLink to='/about'>index to about</NavLink>,
+		});
+		const router = createRouter({
+			history: createMemoryHistory({
+				initialEntries: ["/"],
+			}),
+			routeTree: rootRoute.addChildren([componentRoute]),
+		});
+
+		// Mock server mode
+		// @ts-expect-error
+		router.isServer = true;
+
+		await router.load();
+
+		const link = render(<RouterProvider router={router} />);
+
+		link.debug();
 	});
-	const router = createRouter({
-		history: createMemoryHistory({
-			initialEntries: ["/"],
-		}),
-		routeTree: rootRoute.addChildren([componentRoute]),
-	});
-
-	// Mock server mode
-	// @ts-expect-error
-	router.isServer = true;
-
-	await router.load();
-
-	const link = render(<RouterProvider router={router} />);
-
-	link.debug();
 });
