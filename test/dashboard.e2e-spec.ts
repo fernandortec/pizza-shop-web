842fc185413de149977a432da9c5da5411245266
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-04-21 01:12:31 -0300

    :test_tube: (test) dashboard and orders

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+
+test("should display month cancelled orders amount metric", async ({
+	page,
+}) => {
+	await page.goto("/", { waitUntil: "networkidle" });
+
+	expect(page.getByText("10", { exact: true })).toBeVisible();
+	expect(page.getByText("+2% em relação ao mês passado")).toBeVisible();
+});
+
+test("should display day orders amount metric", async ({ page }) => {
+	await page.goto("/", { waitUntil: "networkidle" });
+
+	expect(page.getByText("20", { exact: true })).toBeVisible();
+	expect(page.getByText("-5% em relação ao dia passado")).toBeVisible();
+});
+
+test("should display month orders amount metric", async ({ page }) => {
+	await page.goto("/", { waitUntil: "networkidle" });
+
+	expect(page.getByText("200", { exact: true })).toBeVisible();
+	expect(page.getByText("-2% em relação ao mês passado")).toBeVisible();
+
+  await page.waitForTimeout(2000)
+});
+
+test("should display month receipt metric", async ({ page }) => {
+	await page.goto("/", { waitUntil: "networkidle" });
+
+	expect(page.getByText("R$ 290,00", { exact: true })).toBeVisible();
+	expect(page.getByText("+10% em relação ao mês passado")).toBeVisible();
+});
